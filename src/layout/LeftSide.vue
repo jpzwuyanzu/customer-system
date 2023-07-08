4c5cfeaff32f3afd9ce23fa159ed116b5c7d52d8
wang <1612000627@qq.com>
2023-07-08 18:51:28 +0800

    搜索功能

--- src/layout/LeftSide.vue
+++ src/layout/LeftSide.vue
@@ -2,68 +2,72 @@
   <el-scrollbar :wrap-style="{ width: '100%' }" :class="Store.counter.themeMode ? 'zt_night' : ''">
     <el-affix :offset="0">
       <div style="padding:16px;z-index: 2;" :class="Store.counter.themeMode ? 'zt_night' : 'bgfff'">
-        <el-input v-model="input1" placeholder="搜索" class="zdy_input" clearable :prefix-icon="Search" :minlength="50" />
+        <el-input v-model="search_val" @input="search_fun" placeholder="搜索" class="zdy_input" clearable
+          :prefix-icon="Search" :minlength="50" />
       </div>
     </el-affix>
-    <div class="chatlist" :class="selec_index == index ? 'cur' : ''" v-for="(item, index) in contacts" :key="index"
-      @click="selectContacts(item, index)" @contextmenu.prevent="rightClick(index)">
-      <el-popover :offset="0" :visible="item.rightprevent" placement="bottom-end" :width="130" trigger="click"
-        :show-arrow="false">
-        <template #reference>
-          <div class="row">
-            <div class="mr10">
-              <el-avatar :size="40" :src="item.avatar" />
-            </div>
-            <div class="ow w">
-              <div class="between mb5">
-                <div class="f14 fw mb5 svh mr5">
-                  {{ item.name }}
+    <div v-for="(item, index) in contacts" :key="index">
+      <div class="chatlist" :class="selec_index == index ? 'cur' : ''" @click="selectContacts(item, index)"
+        @contextmenu.prevent="rightClick(index)" v-show="item.show">
+        <el-popover :offset="0" :visible="item.rightprevent" placement="bottom-end" :width="130" trigger="click"
+          :show-arrow="false">
+          <template #reference>
+            <div class="row">
+              <div class="mr10">
+                <el-avatar :size="40" :src="item.avatar" />
+              </div>
+              <div class="ow w">
+                <div class="between mb5">
+                  <div class="f14 fw mb5 svh mr5">
+                    {{ item.name }}
+                  </div>
+                  <div class="f12 cr999">
+                    {{ timeDisplayMode(item.time) }}
+                  </div>
                 </div>
-                <div class="f12 cr999">
-                  {{ timeDisplayMode(item.time) }}
+                <div class="between">
+                  <div class="f12 svh cr666 mr5" style="width: 100%;">
+                    {{ item.news }}
+                  </div>
+                  <div class="tipsnum" v-if="item.tipsnum != 0">
+                    {{ item.tipsnum }}
+                  </div>
+                  <el-icon style="margin-left: 5px;" v-if="item.istop">
+                    <Upload />
+                  </el-icon>
                 </div>
               </div>
-              <div class="between">
-                <div class="f12 svh cr666 mr5" style="width: 100%;">
-                  {{ item.news }}
-                </div>
-                <div class="tipsnum" v-if="item.tipsnum != 0">
-                  {{ item.tipsnum }}
-                </div>
-                <el-icon style="margin-left: 5px;" v-if="item.istop">
-                  <Upload />
-                </el-icon>
-              </div>
+            </div>
+          </template>
+          <div class="cr707c97 popover_rightcn">
+            <div class="rowC crpr" @click="topping(index)" v-if="!item.istop">
+              <el-icon class="mr10">
+                <Upload />
+              </el-icon>
+              置顶
+            </div>
+            <div class="rowC crpr" @click="offTopping(index)" v-else>
+              <el-icon class="mr10">
+                <Upload />
+              </el-icon>
+              取消置顶
+            </div>
+            <div class="rowC crpr" @click="read(index)">
+              <el-icon class="mr10">
+                <Finished />
+              </el-icon>标记已读
+            </div>
+            <div class="rowC crpr" @click="del(index)">
+              <el-icon class="mr10">
+                <DeleteFilled />
+              </el-icon>删除
             </div>
           </div>
-        </template>
-        <div class="cr707c97 popover_rightcn">
-          <div class="rowC crpr" @click="topping(index)" v-if="!item.istop">
-            <el-icon class="mr10">
-              <Upload />
-            </el-icon>
-            置顶
-          </div>
-          <div class="rowC crpr" @click="offTopping(index)" v-else>
-            <el-icon class="mr10">
-              <Upload />
-            </el-icon>
-            取消置顶
-          </div>
-          <div class="rowC crpr" @click="read(index)">
-            <el-icon class="mr10">
-              <Finished />
-            </el-icon>标记已读
-          </div>
-          <div class="rowC crpr" @click="del(index)">
-            <el-icon class="mr10">
-              <DeleteFilled />
-            </el-icon>删除
-          </div>
-        </div>
-      </el-popover>
+        </el-popover>
 
+      </div>
     </div>
+
   </el-scrollbar>
 </template>
 
@@ -75,7 +79,7 @@ import Mock from 'mockjs';
 import { timeDisplayMode } from '@/utils/common'
 import { ElMessageBox } from 'element-plus'
 const Store = ref(useStore())
-let input1 = ref<any>('')
+let search_val = ref<any>('')
 let selec_index = ref<any>(-1)
 let Mockdata = Mock.mock({
   'list|100': [{
@@ -87,6 +91,7 @@ let Mockdata = Mock.mock({
     'tipsnum|1-100': 0,
     rightprevent: false,
     istop: false,
+    show: true,
   }],
 })
 let contacts = ref<any>(Mockdata.list)
@@ -153,6 +158,27 @@ const read = (index: any) => {
   contacts.value[index].tipsnum = 0
   closeright()
 }
+
+const search_fun = () => {
+  //搜索
+  if (search_val.value == '') {
+    for (let i in contacts.value) {
+      contacts.value[i].show = true
+    }
+  } else {
+    for (let i in contacts.value) {
+      contacts.value[i].show = false
+    }
+  }
+  for (let i in contacts.value) {
+    let arr = contacts.value[i]
+    if (arr.name.includes(search_val.value)) {
+      contacts.value[i].show = true
+    }
+  }
+}
+
+
 
 const del = (index: any) => {
   (ElMessageBox as any)({
@@ -239,4 +265,5 @@ const del = (index: any) => {
     background-color: #F2F2F3;
     border-radius: 5px;
   }
-}</style>
+}
+</style>
